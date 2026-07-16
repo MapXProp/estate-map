3a47e9aeaf623b4df32208f7e6161a6aa6bad37f
MapXProp <sukoon.official@outlook.com>
2026-07-16 12:34:58 +0700

    Add dark theme logo

--- src/shared/LogoSvg.tsx
+++ src/shared/LogoSvg.tsx
@@ -1,5 +1,10 @@
 const LogoSvg = () => {
-  return <img className="block w-full" src="/M5.png" alt="MapxProp" />
+  return (
+    <>
+      <img className="block w-full dark:hidden" src="/M5.png" alt="MapxProp" />
+      <img className="hidden w-full dark:block" src="/M5 dark 2.png" alt="MapxProp" />
+    </>
+  )
 }
 
 export default LogoSvg
