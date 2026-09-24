da71a248331f66e3f5c0cb0596b94bdf1c7fd83d
MapXProp <sukoon.official@outlook.com>
2026-09-24 21:09:25 +0700

    Remove Happening cities section from the blog index

--- src/app/(app)/(other-pages)/blog/page.tsx
+++ src/app/(app)/(other-pages)/blog/page.tsx
@@ -2,7 +2,6 @@ import BgGlassmorphism from '@/components/BgGlassmorphism'
 import SectionAds from '@/components/blog/SectionAds'
 import SectionGridPosts from '@/components/blog/SectionGridPosts'
 import SectionMagazine5 from '@/components/blog/SectionMagazine5'
-import SectionOurFeatures from '@/components/SectionOurFeatures'
 import { getBlogPosts } from '@/data/data'
 import { Metadata } from 'next'
 import React from 'react'
@@ -25,7 +24,6 @@ const BlogPage: React.FC = async () => {
         </div>
         <SectionAds />
         <SectionGridPosts posts={blogPosts} className="py-16 lg:py-28" />
-        <SectionOurFeatures className="mt-16 pb-16 lg:pb-28" />
       </div>
     </div>
   )
